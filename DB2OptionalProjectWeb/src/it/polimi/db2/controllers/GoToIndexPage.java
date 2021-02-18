650ad8b3c369dba2b6b408e93cea8079ea3da492
francescomilanoz <francescomilanozzz@gmail.com>
2021-02-18 16:35:38 +0100

    Update GoToIndexPage.java

--- DB2OptionalProjectWeb/src/it/polimi/db2/controllers/GoToIndexPage.java
+++ DB2OptionalProjectWeb/src/it/polimi/db2/controllers/GoToIndexPage.java
@@ -26,7 +26,7 @@ public class GoToIndexPage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private TemplateEngine templateEngine;
 
-	public GoToIndexPage() {
+	public GoToIndexPage()  {
 		super();
 	}
 
